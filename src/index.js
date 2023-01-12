4ba0648f85c38896a6e0e2657fbf415682eec681
EugeneDubrova777 <blitzkriegbop777@gmail.com>
2023-01-12 12:33:27 +0300

    fix: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  result = +n.toString().split('').reverse().join('');
+
+  if (n < 0) {
+    return +n.toString().split('').reverse().join('').slice(0, -1);
+  } else return +n.toString().split('').reverse().join('');
 }
